ae157afee34843b33363202d5adf3ac085b3343c
rlova <lovaremano@gmail.com>
2025-04-16 13:33:30 +0200

    reservationRestaurant

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -4,6 +4,12 @@ public abstract class Reservation {
 	private int jour;
 	private int mois;
 	
+	public Reservation(int jour, int mois) {
+		super();
+		this.jour = jour;
+		this.mois = mois;
+	}
+
 	@Override
 	public String toString() {
 		return "Le "+jour+"/"+mois;
--- src/model/ReservationRestaurant.java
+++ src/model/ReservationRestaurant.java
@@ -6,8 +6,8 @@ public class ReservationRestaurant extends Reservation {
 	private int numTable;
 	private int numService;
 	
-	public ReservationRestaurant(int jour, int mois, int numTable, int numService) {
-		super();
+	public ReservationRestaurant(int jour, int mois, int numService, int numTable) {
+		super(jour,mois);
 		this.jour = jour;
 		this.mois = mois;
 		this.numTable = numTable;
@@ -16,23 +16,15 @@ public class ReservationRestaurant extends Reservation {
 
 	@Override
 	public String toString() {
-//		StringBuilder chaine = new StringBuilder();
-//		chaine.append(toString());
-//		chaine.append(" : table n°"+numTable+" pour le ");
-//		if (numService==1) {
-//			chaine.append(" premier");
-//		} else if (numService==2) {
-//			chaine.append(" deuxieme");
-//		}
-//		chaine.append(" service");
-//		return chaine.toString();
-		String chaine = super.toString();
-		chaine += " : table n°"+numTable+" pour le ";
+		StringBuilder chaine = new StringBuilder();
+		chaine.append(super.toString());
+		chaine.append("\nTable "+numTable+" pour le ");
 		if (numService==1) {
-			chaine += "premier";
-		} else if (numService==2) {
-			chaine += "deuxieme";
+			chaine.append("premier");
+		} else {
+			chaine.append("deuxieme");
 		}
-		return chaine+" service";
+		chaine.append(" service.");
+		return chaine.toString();
 	}
 }
